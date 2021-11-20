d30120cffd51df600859af292eeba8b9f7c02d9e
windows 7 <windows 7@WINDOWS7-PC>
2021-11-19 21:20:31 -0300

    initial commit

---
+++ ApiCatalogoJogos2/Controllers/V1/JogosController.cs
@@ -0,0 +1,52 @@
+﻿using ApiCatalogoJogos2.InputModel;
+using ApiCatalogoJogos2.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos2.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<JogoViewModel>>> obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> obter(Guid idJogo)
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> inserirJogo(JogoInputModel Jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogos)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
